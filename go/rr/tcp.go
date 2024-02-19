88e3800dfa91efb7bd414aa669672b4f13432993
aesaganda <erensaganda@marun.edu.tr>
2024-02-20 02:12:03 +0300

    added tcp support

--- go/rr/tcp.go
+++ go/rr/tcp.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-    "fmt"
-    "io"
-    "net"
-    "net/http"
-    "net/http/httputil"
-    "net/url"
-    "sync"
-
-    "github.com/gorilla/mux"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"sync"
+    "flag"
+	"github.com/gorilla/mux"
 )
 
 type Server struct {
-    URL        *url.URL
-    ReverseURL *httputil.ReverseProxy
+	URL        *url.URL
+	ReverseURL *httputil.ReverseProxy
 }
 
 var servers []*Server
@@ -22,73 +22,90 @@ var currentIndex int
 var mutex sync.Mutex
 
 func init() {
-    for i := 0; i < 3; i++ {
-        target, _ := url.Parse(fmt.Sprintf("http://%s:%d", "0.0.0.0", 8080+i))
+	serverNames := []string{"localhost"}
+	for i, serverName := range serverNames {
+		target, _ := url.Parse(fmt.Sprintf("http://%s:%d", serverName, 8080+i))
 
-        server := &Server{
-            URL:        target,
-            ReverseURL: httputil.NewSingleHostReverseProxy(target),
-        }
+		server := &Server{
+			URL:        target,
+			ReverseURL: httputil.NewSingleHostReverseProxy(target),
+		}
 
-        servers = append(servers, server)
-    }
+		servers = append(servers, server)
+	}
 }
 
 func getNextServer() *Server {
-    mutex.Lock()
-    defer mutex.Unlock()
+	mutex.Lock()
+	defer mutex.Unlock()
 
-    server := servers[currentIndex%len(servers)]
-    currentIndex++
+	server := servers[currentIndex%len(servers)]
+	currentIndex++
 
-    return server
+	return server
 }
 
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-    server := getNextServer()
-    fmt.Printf("Received connection, forwarding to server at %s\n", server.URL.String())
-    r.Header.Set("X-Forwarded-For", server.URL.Hostname())
-    r.Header.Set("X-Forwarded-Port", server.URL.Port())
-    server.ReverseURL.ServeHTTP(w, r)
+func handleHTTPRequest(w http.ResponseWriter, r *http.Request) {
+	server := getNextServer()
+	fmt.Printf("Received HTTP connection, forwarding to server at %s\n", server.URL.String())
+	r.Header.Set("X-Forwarded-For", server.URL.Hostname())
+	r.Header.Set("X-Forwarded-Port", server.URL.Port())
+	server.ReverseURL.ServeHTTP(w, r)
 }
 
-func handleTCPConnection(conn net.Conn) {
-    defer conn.Close()
+func handleTCPConnection(clientConn net.Conn) {
+	defer clientConn.Close()
 
-    server := getNextServer()
-    fmt.Printf("Received TCP connection, forwarding to server at %s\n", server.URL.String())
+	selectedServer := getNextServer()
+	backendConn, err := net.Dial("tcp", selectedServer.URL.Host)
+	if err != nil {
+		fmt.Println("Error connecting to backend server:", err)
+		return
+	}
+	defer backendConn.Close()
 
-    backendConn, err := net.Dial("tcp", server.URL.Host)
-    if err != nil {
-        fmt.Printf("Error connecting to backend: %v\n", err)
-        return
-    }
-    defer backendConn.Close()
+	// Print TCP Connection Information
+	fmt.Printf("Received TCP connection from %s, forwarding to %s\n",
+		clientConn.RemoteAddr().String(), backendConn.RemoteAddr().String())
 
-    go io.Copy(backendConn, conn)
-    io.Copy(conn, backendConn)
+	go func() {
+		io.Copy(backendConn, clientConn)
+	}()
+
+	go func() {
+		io.Copy(clientConn, backendConn)
+	}()
 }
 
 func main() {
-    go func() {
-        router := mux.NewRouter()
-        router.HandleFunc("/", handleRequest).Methods(http.MethodGet)
+	hostname := flag.String("hostname", "0.0.0.0", "hostname to listen on")
+	httpPort := flag.String("httpport", "8000", "port to listen on for HTTP")
+	tcpPort := flag.String("tcpport", "9000", "port to listen on for TCP")
+	flag.Parse()
 
-        http.ListenAndServe(":8000", router)
-    }()
+	// HTTP Setup
+	router := mux.NewRouter()
+	router.HandleFunc("/", handleHTTPRequest).Methods(http.MethodGet)
+	go func() {
+		fmt.Printf("Starting HTTP load balancer on %s:%s\n", *hostname, *httpPort)
+		http.ListenAndServe(*hostname+":"+*httpPort, router)
+	}()
 
-    listener, err := net.Listen("tcp", ":8001")
-    if err != nil {
-        panic(err)
-    }
+	// TCP Setup
+	tcpListener, err := net.Listen("tcp", *hostname+":"+*tcpPort)
+	if err != nil {
+		fmt.Println("Error starting TCP listener:", err)
+		return
+	}
+	defer tcpListener.Close()
+	fmt.Printf("Starting TCP load balancer on %s:%s\n", *hostname, *tcpPort)
 
-    for {
-        conn, err := listener.Accept()
-        if err != nil {
-            fmt.Printf("Error accepting connection: %v\n", err)
-            continue
-        }
-
-        go handleTCPConnection(conn)
-    }
+	for {
+		conn, err := tcpListener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting TCP connection:", err)
+			continue
+		}
+		go handleTCPConnection(conn)
+	}
 }
---
+++ go/server/server2.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"net/http"
+)
+
+func main() {
+	hostname := flag.String("hostname", "0.0.0.0", "hostname to listen on")
+	httpPort := flag.String("httpport", "8080", "port to listen on for HTTP")
+	tcpPort := flag.String("tcpport", "9090", "port to listen on for TCP")
+	flag.Parse()
+
+	// Start HTTP server in a goroutine
+	go func() {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			forwardedHostname := r.Header.Get("X-Forwarded-For")
+			forwardedPort := r.Header.Get("X-Forwarded-Port")
+
+			// Print received HTTP connection information
+			fmt.Printf("Received HTTP connection from %s:%s\n", forwardedHostname, forwardedPort)
+
+			fmt.Fprintf(w, "Hello from server at Hostname: %s and Port: %s\n", forwardedHostname, forwardedPort)
+		})
+		fmt.Printf("Starting HTTP server on %s:%s\n", *hostname, *httpPort)
+		http.ListenAndServe(*hostname+":"+*httpPort, nil)
+	}()
+
+	// Start TCP listener in a goroutine
+	go func() {
+		listener, err := net.Listen("tcp", *hostname+":"+*tcpPort)
+		if err != nil {
+			fmt.Println("Error starting TCP listener:", err)
+			return
+		}
+		defer listener.Close()
+		fmt.Printf("Starting TCP server on %s:%s\n", *hostname, *tcpPort)
+
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				fmt.Println("Error accepting TCP connection:", err)
+				continue
+			}
+			go handleTCPConnection(conn)
+		}
+	}()
+
+	// Keep the main goroutine alive (optional)
+	select {}
+}
+
+func handleTCPConnection(conn net.Conn) {
+	defer conn.Close()
+	fmt.Printf("Received TCP connection from %s\n", conn.RemoteAddr().String())
+
+	// Add your TCP connection handling logic here
+	// Example: Read data from connection using conn.Read(...)
+}
